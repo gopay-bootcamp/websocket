2b418ae3824682a345e6ddadf28c6b1f38d5abd6
darshak-sorathiya <68641147+darshak-sorathiya@users.noreply.github.com>
2020-08-05 18:12:05 +0530

    Update serverFunctions_test.go

--- server/serverFunctions_test.go
+++ server/serverFunctions_test.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"websocket/client"
+)
+
+func TestSetupListener(t *testing.T) {
+	listener,listenerErr:= SetupListener("tcp", "localhost:49152")
+	assert.Equal(t,nil,listenerErr)
+	listener.Close()
+
+	_,listenerErr=SetupListener("tcp", "localhost:49152403293")
+	assert.NotEqual(t,nil,listenerErr)
+
+}
+
+func TestSetupConnection(t *testing.T) {
+	listener,_:= SetupListener("tcp", "localhost:49153")
+	connectionWithServer,_:=client.DialServer("tcp", "localhost:49153")
+	_,connectionWithClientErr := SetupConnection(listener)
+	assert.Equal(t,nil,connectionWithClientErr)
+	listener.Close()
+	connectionWithServer.Close()
+
+}
+
